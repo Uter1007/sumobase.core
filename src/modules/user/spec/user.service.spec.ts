c754de6bdb63ab86953eb07d61de3a06a08c1c48
Christian Niederreiter <christian@niederreiter.tech>
2016-08-02 22:59:06 +0200

    test(user): first "findUserByUserNameAndPassword" test

--- src/modules/user/spec/user.service.spec.ts
+++ src/modules/user/spec/user.service.spec.ts
@@ -27,6 +27,30 @@ describe('User Service', () => {
         repoMock = sinon.mock(repoObj);
     });
 
+    it('findUserByUserNameAndPassword findOne fails @unit', async () => {
+
+        let error = new Error('The Error');
+
+        loggerMock
+            .expects('error')
+            .withArgs('An error occurred:', error)
+            .once();
+
+        repoMock
+            .expects('findOne')
+            .withArgs({'email': 'the username'})
+            .once()
+            .throws(error);
+
+        let userService = new UserService(loggerMock.object, repoMock.object);
+
+        await userService.findUserByUserNameAndPassword('the username', 'the password');
+
+        loggerMock.verify();
+        repoMock.verify();
+
+    });
+
     it('findUserByName findOne fails @unit', async () => {
 
         let error = new Error('The Error');
