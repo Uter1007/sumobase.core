e5521062ce4f498b5ccadd589d4263f67813481c
Christian Niederreiter <christian@niederreiter.tech>
2016-08-06 22:33:55 +0200

    test(user): further unit tests

--- src/modules/user/spec/user.service.spec.ts
+++ src/modules/user/spec/user.service.spec.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import {UserService} from '../services/user.service';
+import {IUser} from '../interfaces/user.interface';
 
 /* tslint:disable */
 let expect = require('chai').expect;
@@ -19,6 +20,12 @@ describe('User Service', () => {
     };
 
     let repoObj = {
+        create: function(data) {
+            // empty block - just a mock
+        },
+        findById: function(id) {
+            // empty block - just a mock
+        },
         findOne: function(data) {
             // empty block - just a mock
         }
@@ -67,7 +74,6 @@ describe('User Service', () => {
 
     it('findUserByUserNameAndPassword findOne succeeds @unit', async () => {
 
-        let error = new Error('The Error');
         let passwordHash = await hashPassword('the password');
 
         loggerMock
@@ -126,14 +132,96 @@ describe('User Service', () => {
         repoMock
             .expects('findOne')
             .withArgs({'email': 'some@email.address'})
+            .once()
+            .returns({
+                email: 'the username'
+            });
+
+        let userService = new UserService(loggerMock.object, repoMock.object);
+
+        let user = await userService.findUserByName('some@email.address');
+        expect(user.email).to.equal('the username');
+
+        loggerMock.verify();
+        repoMock.verify();
+
+    });
+
+    it('findUserById findById fails @unit', async () => {
+
+        let error = new Error('The Error');
+
+        loggerMock
+            .expects('error')
+            .withArgs('An error occurred:', error)
+            .once();
+
+        repoMock
+            .expects('findById')
+            .withArgs('the user id')
+            .once()
+            .throws(error);
+
+        let userService = new UserService(loggerMock.object, repoMock.object);
+
+        let result = await userService.findUserById('the user id');
+        expect(result).to.equal(error);
+
+        loggerMock.verify();
+        repoMock.verify();
+
+    });
+
+    it('findUserById findById succeeds @unit', async () => {
+
+        loggerMock
+            .expects('error')
+            .never();
+
+        repoMock
+            .expects('findById')
+            .withArgs('the user id')
+            .once()
+            .returns({
+                email: 'the username'
+            });
+
+        let userService = new UserService(loggerMock.object, repoMock.object);
+
+        let user = await userService.findUserById('the user id');
+        expect(user.email).to.equal('the username');
+
+        loggerMock.verify();
+        repoMock.verify();
+
+    });
+
+    it('create create fails @unit', async() => {
+
+        let userModel = <IUser>{};
+        userModel.toDBmodel = sinon.stub().returns({
+            password: hashPassword('the password')
+        });
+
+        let error = new Error('The Error');
+
+        loggerMock
+            .expects('error')
+            .withArgs('An error occurred:', error)
+            .once();
+
+        repoMock
+            .expects('create')
             .once();
 
         let userService = new UserService(loggerMock.object, repoMock.object);
 
-        await userService.findUserByName('some@email.address');
+        // let user =
+        await userService.create(userModel, 'the password');
 
         loggerMock.verify();
         repoMock.verify();
+
     });
 
 });
