bb373589c163eaee8c750d02e18be1735fad07e3
Christoph Ott <christoph.ott@lean-coders.at>
2016-08-26 00:45:42 +0200

    changed mapper for ActionEmail

--- src/modules/commons/activity/mapper/action.email.activity.mapper.ts
+++ src/modules/commons/activity/mapper/action.email.activity.mapper.ts
@@ -5,6 +5,11 @@ import {ActionEmail} from '../models/action.email.activity.model';
 import {UserMapper} from '../../../user/mapper/user.mapper';
 import MAPPER_TAGS from '../../../../constants/mapper.tags';
 import {IUserDBSchema} from '../../../user/models/user.db.model';
+import MongooseMapperHelper from '../../mapper/mongoose.helper';
+
+/* tslint:disable */
+const automapper = require('automapper-ts');
+/* tslint:enable */
 
 @injectable()
 export class ActionEmailMapper {
@@ -16,26 +21,38 @@ export class ActionEmailMapper {
     }
 
     public toDBmodel(model: IActivityEmail): IActivityEmailDBSchema {
-        let userdb: IActivityEmailDBSchema = new activityEmailDBModel({
-            createdOn: model.createdOn,
-            hash: model.hash,
-            id: model.id,
-            state: model.state,
-            type: model.type,
-            user: model.user ? model.user.id : undefined,
-        });
 
-        return userdb;
+        const source = MongooseMapperHelper.getObject<IActivityEmail>(model);
+        const sourceKey = 'IActivityEmail';
+        const destinationKey = 'IActivityEmailDBSchema';
+
+        automapper
+            .createMap(sourceKey, destinationKey)
+            .forMember('user', (opts) => { return opts.sourceObject[opts.sourcePropertyName]
+                                                    ? opts.sourceObject[opts.sourcePropertyName].id
+                                                    : undefined; })
+            .convertToType(activityEmailDBModel);
+
+        return automapper.map(sourceKey, destinationKey, source);
     }
 
     public toActivityEmail(activityModel: IActivityEmailDBSchema, userModel: IUserDBSchema): ActionEmail {
-        return new ActionEmail(activityModel.hash,
-                               activityModel.type,
-                               userModel ? this._userMapper.toUser(userModel) : undefined,
-                               activityModel.id,
-                               activityModel.state,
-                               activityModel.createdOn,
-                               activityModel.modifiedOn);
+
+        const source = MongooseMapperHelper.getObject<IActivityEmailDBSchema>(activityModel);
+        const sourceKey = 'IActivityEmailDBSchema';
+        const destinationKey = 'ActionEmail';
+
+        automapper
+            .createMap(sourceKey, destinationKey)
+            .convertToType(ActionEmail);
+
+        let result: ActionEmail = automapper.map(sourceKey, destinationKey, source);
+
+        if (userModel) {
+            result.user = this._userMapper.toUser(userModel);
+        }
+
+        return result;
     }
 }
 
