c91f34dc42686f1291b50e9328004993727e2bb8
Christoph Ott <christoph.ott@lean-coders.at>
2016-08-26 23:03:00 +0200

    seperated Mapping Configs from Mapping Methods

---
+++ src/constants/models.tags.ts
@@ -0,0 +1,10 @@
+const MODEL_TAGS = {
+    ActivityDBEmail: 'IActivityEmailDBSchema',
+    ActivityEmail: 'IActivityEmail',
+    UserAvatar: 'IUserAvatar',
+    UserDBAvatar: 'IUserAvatarDBSchema',
+    UserDBModel: 'IUserDBSchema',
+    UserModel: 'IUser'
+};
+
+export default MODEL_TAGS;
