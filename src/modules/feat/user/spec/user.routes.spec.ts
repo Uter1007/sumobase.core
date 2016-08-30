0cc8ebb4e4c137a3e29d114cc570b18e3f4159fe
Christian Niederreiter <christian@niederreiter.tech>
2016-08-30 21:52:07 +0200

    test(user): re-introduce supertest-as-promised an add two new tests for the /api/user/check route

--- src/modules/feat/user/spec/user.routes.spec.ts
+++ src/modules/feat/user/spec/user.routes.spec.ts
@@ -4,110 +4,88 @@ let chai = require('chai');
 chai.use(require('chai-as-promised'));
 let expect = chai.expect;
 let sinon = require('sinon');
-let request = require('supertest');
+let request = require('supertest-as-promised');
 let app = require('./fakes/app.fake');
 /* tslint:enable */
 
 describe('User Route Tests', () => {
 
     describe('Special Routes', () => {
-        it('/Login Test', (done) => {
+        it('/Login Test', async () => {
             // noinspection TypeScriptValidateTypes
-            request(app)
+            await request(app)
                 .post('/api/user/login')
                 .type('form')
                 .send({'email': 'christoph.ott@lean-coders.at', 'password': '123appTest$!'})
-                .expect(200)
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        done();
-                    }
-                });
+                .expect(200);
         });
-        it('/Login Test; fails because of wrong password', (done) => {
+        it('/Login Test; fails because of wrong password', async () => {
             // noinspection TypeScriptValidateTypes
-            request(app)
+            await request(app)
                 .post('/api/user/login')
                 .type('form')
                 .send({'email': 'christoph.ott@lean-coders.at', 'password': '123appTest$!.wrong'})
-                .expect(401)
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        done();
-                    }
-                });
+                .expect(401);
         });
-        it('/Login Test; fails because of inexistent user', (done) => {
+        it('/Login Test; fails because of inexistent user', async () => {
             // noinspection TypeScriptValidateTypes
-            request(app)
+            await request(app)
                 .post('/api/user/login')
                 .type('form')
                 .send({'email': 'inexistent.user@somewhere.else', 'password': '123appTest$!.wrong'})
-                .expect(401)
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        done();
-                    }
-                });
+                .expect(401);
         });
 
     });
 
     describe('Restricted Routes Not LoggedIn', () => {
-        it('/me Route Test', (done) => {
+        it('/me Route Test', async () => {
 
             // noinspection TypeScriptValidateTypes
-            request(app)
+            await request(app)
                 .get('/api/user/me')
-                .expect(401)
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        done();
-                    }
-                });
+                .expect(401);
         });
     });
 
     describe('Restricted Routes LoggedIn', () => {
 
         let agent = request.agent(app);
-        beforeEach(function(done){
+        beforeEach(async () => {
             // noinspection TypeScriptValidateTypes
-            agent
+            await agent
                 .post('/api/user/login')
                 .type('form')
-                .send({'email': 'christoph.ott@lean-coders.at', 'password': '123appTest$!'})
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        done();
-                    }
-                });
+                .send({'email': 'christoph.ott@lean-coders.at', 'password': '123appTest$!'});
         });
 
         describe('/me Route Test', () => {
-            it('/Login Test; logged-in + able to call a protected route', (done) => {
+            it('/Login Test; logged-in + able to call a protected route', async () => {
                 // noinspection TypeScriptValidateTypes
-                agent
+                await agent
                     .get('/api/user/me')
-                    .expect(200)
-                    .end(function(err, res) {
-                        if (err) {
-                            throw err;
-                        } else {
-                            done();
-                        }
-                    });
+                    .expect(200);
             });
+        });
+
+    });
+
+    describe('Check', () => {
+
+        it('/Check Test; e-mail address of existing user', async () => {
+            let agent = request.agent(app);
+            await agent
+                .head('/api/user/check')
+                .set('email', 'christoph.ott@lean-coders.at')
+                .expect(404); // 404 = "already in use"
+        });
+
+        it('/Check Test; e-mail address of existing user', async () => {
+            let agent = request.agent(app);
+            await agent
+                .head('/api/user/check')
+                .set('email', 'somebody.else@somewhere.else')
+                .expect(200); // 200 = "doesn't exist"
         });
 
     });
